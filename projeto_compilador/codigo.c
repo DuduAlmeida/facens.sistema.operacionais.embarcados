80ef2f205d9f0e5746b72cef1ef6b799f8185dee
Eduardo Almeida <contato.eduardopex@hotmail.com>
2023-11-05 14:55:06 -0300

    chore: updated compiladorx

---
+++ projeto_compilador/codigo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+int result;
+int a;
+int b;
+a = 3  + 2  ;
+result = 2 * (a * 3  )  ;
+}
---
+++ projeto_compilador/codigo.x
@@ -0,0 +1,7 @@
+{
+    int result ;
+    int a ;
+    int b ;
+    a = 3 + 2 ;
+    result = 2 * ( a * 3 ) ;
+}
