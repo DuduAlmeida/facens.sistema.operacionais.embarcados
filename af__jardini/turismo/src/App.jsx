5a5d6ace9b02d10d4e576ce184144e3c9d58d560
LucasAbreuFG <lucasdeafg@yahoo.com.br>
2023-12-03 18:53:37 -0300

    feat: tela de registro finalizada e tanto registro como login acessando API

--- af__jardini/turismo/src/App.jsx
+++ af__jardini/turismo/src/App.jsx
@@ -9,12 +9,14 @@ import Header from "./components/Header";
 import HomePage from "./pages/Home";
 import MinePage from "./pages/Mine";
 import Login from "./pages/Login/login";
+import SignUp from "./pages/Register/register";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" Component={Header}>
       <Route index Component={HomePage} />
       <Route path="/login" Component={Login} />
+      <Route path="/register" Component={SignUp} />
       <Route path="/compras" Component={MinePage} />
     </Route>
   )
